b44bfdcfbdb77a503804241d89202f4ba69d2aba
Bruno Rocha <mail.brunorocha@gmail.com>
2019-07-02 11:00:30 -0300

    Initial Commit

---
+++ core-ml-project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  core-ml-project
+//
+//  Created by Bruno Rocha on 02/07/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
